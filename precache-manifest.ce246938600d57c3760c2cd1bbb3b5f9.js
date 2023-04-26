3569b7f22b13b5854a19cd07c013091ac3b9b0f2
PujalaSuvarna <102460349+PujalaSuvarna@users.noreply.github.com>
2023-04-27 01:44:55 +0530

    Updates

---
+++ precache-manifest.ce246938600d57c3760c2cd1bbb3b5f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "df4a21f794710693903f",
+    "url": "/static/css/main.8f7553ba.chunk.css"
+  },
+  {
+    "revision": "df4a21f794710693903f",
+    "url": "/static/js/main.df4a21f7.chunk.js"
+  },
+  {
+    "revision": "ffcfc661787b4f8ad0f7",
+    "url": "/static/js/1.ffcfc661.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "17f53687e48bbf15546cb2324e2a44a5",
+    "url": "/index.html"
+  }
+];
